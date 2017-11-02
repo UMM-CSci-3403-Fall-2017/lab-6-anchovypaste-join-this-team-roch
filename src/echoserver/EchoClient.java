6118e0bb8eb280aeae1d9ab8e380feec834323b3
Kyle M Foss <fossx229@normandy.morris.umn.edu>
2017-11-02 11:46:34 -0500

    --Started initial work on lab 6.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,6 +1,31 @@
-package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+  public static void main(String[] args) {
+    try {
+      Socket socket = new Socket("127.0.0.1", 6013);
+      InputStream input;
+      OutputStream output;
 
+      File file = new File(args[0]);
 
+      byte[] bytes = new byte[16 * 1024];
+      output = socket.getOutputStream();
+      input = socket.getInputStream();
+
+      int count;
+        while ((count = input.read(bytes)) > 0) {
+        	output.write(bytes, 0, count);
+        }
+
+      input.close();
+      output.close();
+      socket.close();
+
+    } catch (IOException ioe) {
+      System.out.println("We caught an exception");
+      System.err.println(ioe);
+    }
+  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,17 +1,51 @@
-package echoserver;
-
 import java.net.*;
 import java.io.*;
 
 public class EchoServer {
-	public static void main(String[] args) {
-		try {
-			ServerSocket sock = new ServerSocket(6013);
+  public static void main(String[] args) {
+    try {
+      Socket socket = null;
+      InputStream input = null;
+      OutputStream output = null;
+      ServerSocket sock = new ServerSocket(6013);
 
-			while(true) {
-				Socket client = sock.accept();
+      //Code gotten from stackOverFlow userName = Pankaj
+      while (true) {
+        System.out.println("Got a request!");
+        try {
+            socket = sock.accept();
+        } catch (IOException ex) {
+            System.out.println("Can't accept client connection. ");
+        }
 
-			}
-		}
-	}
+        try {
+            input = socket.getInputStream();
+            System.out.println(input);
+            System.out.println("here");
+        } catch (IOException ex) {
+            System.out.println("Can't get socket input stream. ");
+        }
+
+        try {
+            output = new FileOutputStream("M:\\test2.xml");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found. ");
+        }
+
+        byte[] bytes = new byte[16*1024];
+
+        int count;
+        while ((count = input.read(bytes)) > 0) {
+            output.write(bytes, 0, count);
+        }
+
+        output.close();
+        input.close();
+        socket.close();
+        sock.close();
+      }
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
 }
